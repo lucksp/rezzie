1177dea194cb5c80e7dc805c841220a57235983f
Phil Lucks <philip.lucks@snapengage.com>
2020-11-28 10:08:28 -0700

    install deps for parcel

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import { render } from 'react-dom';
+
+const Application = (): ReactElement => (
+  <h1>Application</h1>
+);
+
+render(<Application />, document.getElementById('root'));
